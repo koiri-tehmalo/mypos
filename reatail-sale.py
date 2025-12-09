3442eea1fe1bd603ed2a4f7a324ef344daec9480
koiri-tehmalo <khayreetehmalo@gmail.com>
2025-12-09 13:30:34 +0700

    แก้เงินสด

--- reatail-sale.py
+++ reatail-sale.py
@@ -142,16 +142,20 @@ def test_retail_sales_2(config):
     except Exception as e:
         print(f"[X] Error during Retail Sales Test 2: {e}")
 
-def payment1():
+def payment1(config):
     print("Payment 1 executed")
     # ดึงค่าที่เกี่ยวข้องกับ Payment
-    PM_CFG = CONFIG['PAYMENT_AMOUNT']
+    PM_CFG = CONFIG['PAYMENT']
 
     try:
         app = Application(backend="uia").connect(title_re=WINDOW_TITLE, timeout=10)
         main_window = app.top_window()
-        main_window.child_window(title=PM_CFG['CASH_TITLE'], auto_id=RS_CFG['HOTKEY_R_AUTO_ID'], control_type="Text").click_input()
+        main_window.child_window(title=PM_CFG['CASH_TITLE'], auto_id=PM_CFG['PAYMENT_AUTO_ID'], control_type="Text").click_input()
         time.sleep(SLEEP_TIME)
+        main_window.type_keys(f"{PM_CFG['CASH_AMOUNT']}{{ENTER}}")
+        time.sleep(SLEEP_TIME)
+        main_window.child_window(title=config['GLOBAL']['ABORT_PRINT_TITLE'], auto_id=config['GLOBAL']['ABORT_PRINT_AUTO_ID'], control_type="Button").click_input()
+
         
     except Exception as e:
         print(f"[X] Error during Payment 1: {e}")
